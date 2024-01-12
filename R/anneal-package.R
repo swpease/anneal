462b7f95cfe4132a66c01d4805f1b1f1e0735f14
swpease <swpease@gmail.com>
2024-01-12 15:59:59 -0800

    initial commit
    Set-up project as a "project".

---
+++ R/anneal-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @import assertthat
+#' @import dplyr
+#' @import ggplot2
+#' @import lubridate
+#' @import magrittr
+#' @importFrom rlang :=
+#' @import tsibble
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
